1ce1cc1bc5b50b2ea7b93b3fcd32cb991db0c199
Adam Wadsworth <32913685+highfly117@users.noreply.github.com>
2024-03-03 23:46:33 +0000

    actually fixing it

---
+++ src/componants/DataGridTable.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Propertybanner from "./Propertybanner"; // Ensure this matches the file and component name exactly
+import "../CSS/DataGridTable.css";
+
+const DataGridTable = ({
+    data,
+    onSelectItem
+}) => {
+    const [activeBanner, setActiveBanner] = useState(null);
+
+    const toggleActiveBanner = (id) => {
+        setActiveBanner(activeBanner === id ? null : id);
+        onSelectItem(data[id]); // Assuming onSelectItem is designed to handle the selected item
+    };
+
+    return (
+        <div style={{ width: "50%", marginRight: "30px" }} className="dataTable">
+            {data.slice(0, 36).map((item, index) => {
+                console.log(item); // Log each item as it's rendered
+                return (
+                    <Propertybanner
+                        key={item._id}
+                        imageUrl1={item.ImageLink}
+                        imageUrl2={item.PhotoURLs}
+                        priceType={item.Price.Type}
+                        price={`£${item.Price.Value}`}
+                        size={`${item.Space}m2`}
+                        pricePerMeter={`£${(item.Price.Value / item.Space).toFixed(0)}/m2`}
+                        address={item.Address}
+                        description={`Semi Detached - ${item.Bedrooms} Bed - ${item.Bathrooms} Bath - ${item.Sittingrooms} Sitting`}
+                        addedDate={item.TimeStamp.slice(0, 10)}
+                        viewLink={item.URL}
+                        isActive={activeBanner === index}
+                        toggleClass={() => toggleActiveBanner(index)}
+                        onClick={() => onSelectItem(item)}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default DataGridTable;
